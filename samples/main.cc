9d7ae0014b8f478738961c2b955753095761e49c
Sashank Reddy Appireddy <sshnk.82@gmail.com>
2016-08-22 16:18:03 -0700

    Structure Packing and Padding BugID: ReviewID: CC: Reviewed-By: Approved-By:

--- samples/main.cc
+++ samples/main.cc
@@ -62,11 +62,40 @@ test4(void)
    return 1;
 }
 
+void test5(void)
+{
+    /* Size of a structure */
+    cout << "Started Test: " << __func__ << endl;
+    btnode_t a,b;
+    uint8_t *a_,*b_;
+    a_ = (uint8_t*)&a;
+    b_ = (uint8_t*)&b;
+    cout << "Size of btnode_t is " << (b_ - a_) << endl;
+
+    typedef struct sample1 {
+        int h;
+        char a;
+        char b;
+        void *x;
+    } __attribute__ ((packed)) sample1_t;
+
+    typedef struct sample2 {
+        int h;
+        char a;
+        char b;
+        void *x;
+    } sample2_t;
+
+    cout << "size of sample struct with packing is "<< sizeof(sample1_t) << endl;
+    cout << "size of sample struct without packing but gets padded is "<< sizeof(sample2_t) << endl;
+}
+
 int main()
 {
    test1();
    test2();
    test3();
    test4();
+   test5();
    return 1;
 } 
